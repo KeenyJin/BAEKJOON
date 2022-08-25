05118fd22f09602c743216d54253949ad862a850
_/ <111881743+KeenyJin@users.noreply.github.com>
2022-08-26 05:10:47 +0900

    Dynamic Programming 1 - Longest Increasing Subsequence

---
+++ 11053.py
@@ -0,0 +1,28 @@
+cache = {}
+N = int(input())
+input_lst = list(map(int, input().split()))
+
+
+def maxstr(x: list) -> int:
+    if tuple(x) in cache:
+        pass
+    elif len(x) == 1:
+        cache[tuple(x)] = 1
+    else:
+        max_result = 1
+        for i in range(1, len(x)):
+            if x[0] < x[i]:
+                if max_result < 1 + maxstr(x[i:]):
+                    max_result = 1 + maxstr(x[i:])
+        cache[tuple(x)] = max_result
+    return cache[tuple(x)]
+
+
+min_idx = 0
+max_ans = maxstr(input_lst)
+for i in range(1, len(input_lst)):
+    if input_lst[i] < input_lst[min_idx]:
+        min_idx = i
+        if max_ans < maxstr(input_lst[i:]):
+            max_ans = maxstr(input_lst[i:])
+print(max_ans)
